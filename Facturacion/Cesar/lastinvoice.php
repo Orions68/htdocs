9ce59411aae9677bf0150cc8866f038d39a5b984
Orions68 <orions68@gmail.com>
2023-02-15 18:45:16 +0000

    Good PDF

--- Facturacion/Cesar/lastinvoice.php
+++ Facturacion/Cesar/lastinvoice.php
@@ -70,7 +70,7 @@ $time = $row->time;
                         </div>';
 					?>
                     <br><br>
-                    <button class="btn btn-danger" onclick="window.close()">Cierra Esta Ventana</button>
+                    <button class="btn btn-danger btn-lg" onclick="window.close()">Cierra Esta Ventana</button>
                     <br><br><br><br><br>
 				</div>
             </div>
